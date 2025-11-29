62ba4e50009c6514f845e6f2642c47c018405059
thispedroo <jf.pedro09@gmail.com>
2025-11-28 23:02:34 -0300

    creating a function to add tasks and updating existing code

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -1,3 +1,12 @@
+def adicionar_tarefa(tarefas, nome_tarefa):
+  # Tarefa: nome da tarefa
+  # Completada: indicar se essa tarefa já foi completada ou não
+  tarefa = {"tarefa": nome_tarefa, "completada": False}
+  tarefas.append(tarefa)
+  print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
+  return
+
+tarefas = []
 while True:
   print("\nMenu do Gerenciador de Lista de tarefas:")
   print("1. Adicionar tarefa.")
@@ -9,7 +18,10 @@ while True:
 
   escolha = input("Digite a sua escolha: ")
 
-  if escolha == "6":
+  if escolha == "1":
+    nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
+    adicionar_tarefa(tarefas, nome_tarefa)
+  elif escolha == "6":
     break
   
 print("Programa finalizado!")
