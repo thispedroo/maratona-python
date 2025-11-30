0fa490fdeede819fa4ab4357ac2c2d15947ddb32
thispedroo <jf.pedro09@gmail.com>
2025-11-29 23:31:28 -0300

    building the project task addition function

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -6,6 +6,13 @@ def adicionar_tarefa(tarefas, nome_tarefa):
   print(f"Tarefa {nome_tarefa} foi adicionada com sucesso!")
   return
 
+def ver_tarefas(tarefas):
+  print("\nLista de tarefas:")
+  for indice, tarefa in enumerate(tarefas, start=1):
+    status = "✓" if tarefa["completada"] else " "
+    nome_tarefa = tarefa["tarefa"]
+    print(f"{indice}. [{status}] {nome_tarefa}")
+
 tarefas = []
 while True:
   print("\nMenu do Gerenciador de Lista de tarefas:")
@@ -21,6 +28,8 @@ while True:
   if escolha == "1":
     nome_tarefa = input("Digite o nome da tarefa que deseja adicionar: ")
     adicionar_tarefa(tarefas, nome_tarefa)
+  elif escolha == "2":
+    ver_tarefas(tarefas)  
   elif escolha == "6":
     break
   
