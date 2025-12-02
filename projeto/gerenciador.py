25abdb337a2943ab0212b08222b3188c1f63dd4b
thispedroo <jf.pedro09@gmail.com>
2025-12-02 17:50:05 -0300

    adding the function to delete tasks to the project

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -29,6 +29,13 @@ def completar_tarefa(tarefas, indice_tarefa):
   print(f"Tarefa {indice_tarefa} marcada como completada.")
   return
 
+def deletar_tarefas_completadas(tarefas):
+  for tarefa in tarefas:
+    if tarefa["completada"]:
+      tarefas.remove(tarefa)
+  print("Tarefas completadas foram deletadas!")      
+  return
+
 tarefas = []
 while True:
   print("\nMenu do Gerenciador de Lista de tarefas:")
@@ -58,6 +65,11 @@ while True:
     ver_tarefas(tarefas)
     indice_tarefa = input("Digite o número da tarefa que deseja completar: ")
     completar_tarefa(tarefas, indice_tarefa)
+
+
+  elif escolha == "5":
+    deletar_tarefas_completadas(tarefas)
+    ver_tarefas(tarefas)
 
   elif escolha == "6":
     break
