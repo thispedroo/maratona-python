dfa27fe54e2aeda0f7eab91c0a5e05d117eb2b38
thispedroo <jf.pedro09@gmail.com>
2025-12-03 23:11:13 -0300

    learning about tertiary modules

---
+++ tipos_variaveis/modulo_terceiro.py
@@ -0,0 +1,6 @@
+print("\nImportação e uso de um módulo de terceiros")
+import requests
+
+url = "https://www.example.com"
+response = requests.get(url)
+print(f"Solicitação HTTP para {url} retornou o status {response.status_code}")
