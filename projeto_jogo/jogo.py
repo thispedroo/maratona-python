64da84e9ac19bb877bbe4771c794d91dcd5c9de2
thispedroo <jf.pedro09@gmail.com>
2025-12-22 19:31:21 -0300

    improving the project's damage system

--- projeto_jogo/jogo.py
+++ projeto_jogo/jogo.py
@@ -1,3 +1,5 @@
+import random 
+
 # Personagem: Classe mãe
 # Herói: Controlado pelo usuário
 # Inimigo: Adversário do usuário
@@ -26,7 +28,7 @@ class Personagem:
             self.__vida = 0
 
     def atacar(self, alvo):
-        dano = self.__nivel * 2
+        dano = random.randint(self.get_nivel() * 2, self.get_nivel() * 4) # Baseado no nível
         alvo.receber_ataque(dano)
         print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
     
@@ -42,7 +44,7 @@ class Heroi(Personagem):
         return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
     
     def ataque_especial(self, alvo):
-        dano = self.get_nivel() * 5 # Dano aumentado
+        dano = random.randint(self.get_nivel() * 5, self.get_nivel() * 8) # Dano aumentado
         alvo.receber_ataque(dano)
         print(f"{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano!")
 
